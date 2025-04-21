45982f2456b23a51516fa434a1dd3a51aa81466e
Matias Zapf <mzapf@pm.me>
2025-04-21 14:30:44 -0300

    feat: add icons and manifest for improved PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/icons/icon-64x64.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/icons/icon-128x128.png" />
+        <link rel="icon" type="image/png" sizes="256x256" href="/icons/icon-256x256.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512x512.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
